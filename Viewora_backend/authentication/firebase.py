0b2270fc8963960b5a0786377743d60908941b95
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2026-01-29 11:12:52 +0530

    fix: Securely inject Firebase credentials in production environment

--- Viewora_backend/authentication/firebase.py
+++ Viewora_backend/authentication/firebase.py
@@ -5,13 +5,17 @@ from django.conf import settings
 from firebase_admin import credentials, messaging
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(
-        os.path.join(
-            settings.BASE_DIR,
-            "firebase/viewora-notification-firebase-adminsdk-fbsvc-c369f555cf.json",
-        )
+    # Use environment variable for the path, fallback to the default relative path
+    firebase_cred_path = os.getenv(
+        "FIREBASE_CREDENTIALS",
+        os.path.join(settings.BASE_DIR, "firebase/firebase.json")
     )
-    firebase_admin.initialize_app(cred)
+    
+    try:
+        cred = credentials.Certificate(firebase_cred_path)
+        firebase_admin.initialize_app(cred)
+    except Exception as e:
+        print(f"Error initializing Firebase with {firebase_cred_path}: {e}")
 
 
 def send_push_notification(token, title, body, data=None):
