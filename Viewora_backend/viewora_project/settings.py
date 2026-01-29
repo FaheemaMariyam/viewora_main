b72794fabf87133e5f5813ff63f5e8e422726d69
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2026-01-29 14:25:35 +0530

    fix: Enable static files serving with Caddy and automate collectstatic

--- Viewora_backend/viewora_project/settings.py
+++ Viewora_backend/viewora_project/settings.py
@@ -201,6 +201,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
